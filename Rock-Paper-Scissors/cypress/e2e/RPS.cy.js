418c308f81f8cb524fe2f1c7d2c527df0ab9e6d4
Nieves Mena Couso <nmcouso@gmail.com>
2023-03-09 10:06:11 +0000

    Test working

--- Rock-Paper-Scissors/cypress/e2e/RPS.cy.js
+++ Rock-Paper-Scissors/cypress/e2e/RPS.cy.js
@@ -3,26 +3,25 @@ describe('Rock, Paper, Scissors Game', () => {
   beforeEach(()=> {
     cy.visit('Rock, Paper, Scissors!.html')
   });
+  
   // User plays with Rock
   it('play with Rock', () => {
     // Click the "Rock" button
     cy.get('[data-cy=rock]').should("exist").click();
     // Check that the user option is "rock"
-    cy.get('[data-cy=userChoice]').invoke('val').should("contain", "Rock"); 
+    cy.get('[data-cy=userChoice]').should("have.value", "Rock"); 
     //Check for computer option
-    cy.get('[data-cy="computerChoice"]').then(($computerChoice) => {
+    cy.get('[data-cy=computerChoice]').then(($computerChoice) => {
       // If the computer option is set to "rock"
-      if ($computerChoice.text().includes("Rock")) {
+      if ($computerChoice.val()=="Rock") {
         // The result is a tie
-        cy.get('[data-cy=result]').should("contain", "You tie!");
-      // If the computer option is set to "paper"
-      } else if ($computerChoice.text().includes("Paper")) { 
+        cy.get('[data-cy=result]').should("have.value", "You tie!");
+      } else if ($computerChoice.val()=="Paper") {
         // The user looses
-        cy.get('[data-cy=result]').should("contain", "Boo loser!");
-      // If the computer option is set to "scissors"
-      } else if ($computerChoice.text().includes("Scissors")) {
+        cy.get('[data-cy=result]').should("have.value", "Boo loser!");
+      } else {
         // The user wins
-        cy.get('[data-cy=result]').should("contain","You win!");
+        cy.get('[data-cy=result]').should("have.value", "You win!");
       }
     });
   });
@@ -32,21 +31,21 @@ describe('Rock, Paper, Scissors Game', () => {
     // Click the "Paper" button
     cy.get('[data-cy="paper"]').should("exist").click();
     // Check that the user option is "paper"
-    cy.get('[data-cy="userChoice"]').invoke('val').should("contain", "Paper");
+    cy.get('[data-cy="userChoice"]').should("have.value", "Paper");
     // Check for computer option
     cy.get('[data-cy="computerChoice"]').then(($computerChoice) => {
       // If the computer option is set to "rock"
-      if($computerChoice.text().includes("Rock")){
+      if ($computerChoice.val()=="Rock") {
         // The user wins
-        cy.get('[data-cy="result"]').contains("You win!");
+        cy.get('[data-cy="result"]').should("have.value", "You win!");
       // If the computer option is set to "paper"
-      } else if ($computerChoice.text().includes("Paper")){ 
+      } else if ($computerChoice.val()=="Paper") {
         // The result is a tie
-        cy.get('[data-cy="result"]').contains("You tie!");
+        cy.get('[data-cy="result"]').should("have.value", "You tie!");
       // If the computer option is set to "scissors"
-      } else if ($computerChoice.text().includes("Scissors")){
+      } else {
         // The user looses
-        cy.get('[data-cy="result"]').contains("Boo loser!");
+        cy.get('[data-cy="result"]').should("have.value", "Boo loser!");
       }
     });
   });
@@ -56,21 +55,21 @@ describe('Rock, Paper, Scissors Game', () => {
     // Click the "Scissors" button
     cy.get('[data-cy="scissors"]').should("exist").click();
     // Check that the user option is "scissors"
-    cy.get('[data-cy="userChoice"]').invoke('val').should("contain", "Scissors");
+    cy.get('[data-cy="userChoice"]').should("have.value", "Scissors");
     // Check for computer option
     cy.get('[data-cy="computerChoice"]').then(($computerChoice) => {
       // If the computer option is set to "rock"
-      if($computerChoice.text().includes("Rock")){
+      if ($computerChoice.val()=="Rock") {
         // The user looses
-        cy.get('[data-cy="result"]').contains("Boo loser!");
+        cy.get('[data-cy="result"]').should("have.value", "Boo loser!");
       // If the computer option is set to "paper"
-      } else if ($computerChoice.text().includes("Paper")){ 
+      } else if ($computerChoice.val()=="Paper") { 
         // The user wins
-        cy.get('[data-cy="result"]').contains("You win!");
+        cy.get('[data-cy="result"]').should("have.value", "You win!");
       // If the computer option is set to "paper"
-      } else if ($computerChoice.text().includes("Scissors")){
+      } else {
         // The result is a tie
-        cy.get('[data-cy="result"]').contains("You tie!");
+        cy.get('[data-cy="result"]').should("have.value", "You tie!");
       }
     });
   });
